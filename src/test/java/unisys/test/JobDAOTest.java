c6ad6041544031e6ffe80b460c4920c133069b28
wfpena <wilsonf.pena@gmail.com>
2018-04-15 17:28:15 -0300

    Removing unused imports

--- src/test/java/unisys/test/JobDAOTest.java
+++ src/test/java/unisys/test/JobDAOTest.java
@@ -5,11 +5,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.File;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
